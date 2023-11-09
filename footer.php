889932813b689effb73c9479e27eca00fc8dd354
babs95 <babsco95@gmail.com>
2023-11-09 18:12:23 +0000

    ajout bootstrap & jquery dataTable

---
+++ footer.php
@@ -0,0 +1,25 @@
+<footer class="footer mt-auto py-3 bg-body-tertiary">
+  <div class="container">
+    <span class="text-body-secondary">@ GROUPE ISI <?php echo date('Y'); ?></span>
+  </div>
+</footer>
+<!-- <script src="https://getbootstrap.com/docs/5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
+<script src="js/bootstrap.bundle.min.js"></script>
+<script src="DataTables/datatables.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#myDataTable').DataTable({
+            "oLanguage": {
+                "sSearch": "Rechercher",
+                "sLengthMenu": "Afficher _MENU_ Lignes par page",
+                "sInfo": "Affichage de _START_ à _END_ sur _TOTAL_ enregistrements",
+                "oPaginate": {
+                    "sNext": "Suivant",
+                    "sPrevious": "Précédent"
+                }
+            }
+        });
+    });
+</script>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    include_once './header.php'
+?>
+
+<!-- Begin page content -->
+<main class="flex-shrink-0">
+  <div class="container">
+    <h1 class="mt-5">Dalal Ak Diam</h1>
+  </div>
+</main>
+
+<?php
+    include_once './footer.php'
+?>
